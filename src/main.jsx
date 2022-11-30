f3a9759fa6a0aa3076f793cebc3ff1316b123356
ersin-ahmed <ersinahmeddev@gmail.com>
2022-11-30 16:43:24 +0200

    built static site for deployemne

--- src/main.jsx
+++ src/main.jsx
@@ -4,7 +4,7 @@ import App from './App';
 import './index.css';
 import axios from 'axios';
 
-axios.defaults.baseURL = 'http://localhost:5000';
+axios.defaults.baseURL = import.meta.env.VITE_SERVER_URI;
 axios.defaults.withCredentials = true;
 
 ReactDOM.createRoot(document.getElementById('root')).render(<App />);
